1739013eb726259e1602771d91310de952a96965
nggoong <nggoong@naver.com>
2021-11-30 15:30:30 +0900

    마지막 스크롤섹션 컨버스 크기 계산(playAnimation함수에서 수정)및 캔버스에 이미지 넣기 완료✔

--- apple-clone-v10/js/index.js
+++ apple-clone-v10/js/index.js
@@ -98,7 +98,15 @@
             heightNum:5, // 브라우저 높이의 5배로 scrollHeight 세팅
             scrollHeight: 0,
             objs: {
-                container:document.querySelector('#scroll-section-3')
+                container:document.querySelector('#scroll-section-3'),
+                canvasCaption: document.querySelector('.canvas-caption'),
+                canvas:document.querySelector('.image-blend-canvas'),
+                context: document.querySelector('.image-blend-canvas').getContext('2d'),
+                imagePath: [
+                    './images/blend-image-1.jpg',
+                    './images/blend-image-2.jpg',
+                ],
+                images: []
             }
         },
     ];
@@ -113,9 +121,16 @@
 
         let imgElem2;
         for(let i = 0; i < sceneInfo[2].values.videoImageCount; i++) {
-            imgElem = new Image();
-            imgElem.src=`./video/002/IMG_${7027 + i}.JPG`;
-            sceneInfo[2].objs.videoImages.push(imgElem);
+            imgElem2 = new Image();
+            imgElem2.src=`./video/002/IMG_${7027 + i}.JPG`;
+            sceneInfo[2].objs.videoImages.push(imgElem2);
+        }
+
+        let imgElem3;
+        for(let i = 0; i< sceneInfo[3].objs.imagePath.length; i++) {
+            imgElem3 = new Image();
+            imgElem3.src=sceneInfo[3].objs.imagePath[i];
+            sceneInfo[3].objs.images.push(imgElem3);
         }
     }
 
@@ -283,6 +298,24 @@
                 break;
             case 3:
                 // console.log('3 play');
+                // 가로, 세로 모두 꽉 차게 하기 위해 여기서 세팅(계산이 필요)
+                const widthRatio = window.innerWidth / objs.canvas.width;
+                const heightRatio = window.innerHeight / objs.canvas.height;
+                let canvasScaleRatio;
+
+                if(widthRatio <= heightRatio) {
+                    // 캔버스보다 브라우저 창이 홀쭉한 경우
+                    canvasScaleRatio = heightRatio;
+                    console.log('heightRatio로 결정');
+                }
+                else {
+                    // 캔버스보다 브라우저 창이 납작한 경우
+                    canvasScaleRatio = widthRatio;
+                    console.log('widthRatio로 결정');
+                }
+
+                objs.canvas.style.transform = `scale(${canvasScaleRatio})`;
+                objs.context.drawImage(objs.images[0], 0, 0);
                 break;
         }
     }
